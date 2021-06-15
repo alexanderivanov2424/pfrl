5ffbb1850d008f364ad34827b20819a384b12b31
alexanderivanov2424 <alexander_ivanov@brown.edu>
2021-06-15 09:44:37 -0700

    added RND Model

--- pfrl/nn/rnd.py
+++ pfrl/nn/rnd.py
@@ -9,6 +9,79 @@ from pfrl.initializers import init_xavier_uniform
 from pfrl.nn import EmpiricalNormalization
 from pfrl.env import VectorEnv, Env
 
+class RNDModel(nn.Module):
+    """
+    Torch RND architecture taken from https://github.com/jcwleo/random-network-distillation-pytorch/blob/master/model.py
+    """
+
+    def __init__(self, input_size, output_size=512):
+        super(RNDModel, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+
+        feature_output = 7 * 7 * 64
+        self.predictor = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=8,
+                stride=4),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=4,
+                stride=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1),
+            nn.LeakyReLU(),
+            Flatten(),
+            nn.Linear(feature_output, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512)
+        )
+
+        self.target = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=8,
+                stride=4),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=4,
+                stride=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1),
+            nn.LeakyReLU(),
+            Flatten(),
+            nn.Linear(feature_output, 512)
+        )
+
+        for p in self.modules():
+            if isinstance(p, nn.Conv2d):
+                init.orthogonal_(p.weight, np.sqrt(2))
+                p.bias.data.zero_()
+
+            if isinstance(p, nn.Linear):
+                init.orthogonal_(p.weight, np.sqrt(2))
+                p.bias.data.zero_()
+
+
+
 class RND(torch.nn.Module):
     """Implements Random Network Distillation.
 
@@ -32,8 +105,8 @@ class RND(torch.nn.Module):
         self.init_steps = init_steps
         self.obs_shape = obs_shape
 
-        self.target = model
-        self.predictor = deepcopy(self.target)
+        self.target = model.predictor
+        self.predictor = model.target
 
         self.target.apply(init_xavier_uniform)
         self.predictor.apply(init_xavier_uniform)
