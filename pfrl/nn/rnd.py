2cd46fcf7bfaaf844e08211bb28f690c485d4410
alexanderivanov2424 <alexander_ivanov@brown.edu>
2021-06-18 18:18:54 -0700

    test

--- pfrl/nn/rnd.py
+++ pfrl/nn/rnd.py
@@ -156,6 +156,7 @@ class RND(torch.nn.Module):
             raise ValueError("{} env type not recognized".format(type(env)))
 
     def forward(self, states, update_params=False, log=True):
+        print('rnd entry')
         states = torch.cat([torch.from_numpy(s.__array__(dtype=float)) for s in states]).to(self.device)
         states = self.obs_normalizer(states)
 
